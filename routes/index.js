a04e5162ba17c762f979faabf625490981d03f66
Kerry <wr_asdf@163.com>
2012-11-20 21:01:39 +0800

    add about & contact jade template

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
+};
+
+exports.about = function(req, res){
+  res.render('about');
+};
+
+exports.contact = function(req, res){
+  res.render('contact');
 };
