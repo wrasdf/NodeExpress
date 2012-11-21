60e889d819681160ce0692c2dd45fe5a017eb0e1
Kerry <wr_asdf@163.com>
2012-11-21 12:09:17 +0800

    add new files

---
+++ routes/routes.js
@@ -0,0 +1,8 @@
+
+var routes = require("../routes/index.js");
+
+module.exports = function(app){
+	app.get('/', routes.index);
+	app.get('/about', routes.about);
+	app.get('/contact', routes.contact);
+}
